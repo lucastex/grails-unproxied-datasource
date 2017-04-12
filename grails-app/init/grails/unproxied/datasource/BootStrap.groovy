ef6c0dc56ae4f06e0b4c6dacdd6ebbb09efb6c1b
lucastex <lucas@blanq.com.br>
2017-04-11 23:49:02 -0300

    added - initial data to bootstrap

--- grails-app/init/grails/unproxied/datasource/BootStrap.groovy
+++ grails-app/init/grails/unproxied/datasource/BootStrap.groovy
@@ -3,6 +3,10 @@ package grails.unproxied.datasource
 class BootStrap {
 
     def init = { servletContext ->
+
+        new Contact(name: "Big Ben Roethlisberger", email: "theone@bigbenland.com.br").save()
+        new Contact(name: "Odell Beckham Jr", email: "obj@nygiants.com").save()
+        new Contact(name: "Aaron Rodgers", email: "arodgers@gopackers.com").save()
     }
     def destroy = {
     }
